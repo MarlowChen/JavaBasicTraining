b0802471c2016af0775d229688704a7a4fefae14
marlow <marlow@192.168.0.34>
2019-07-06 11:14:08 +0800

    [java-basic-training]basic java and variable introduction

--- src/javabasictraining/JavaBasicTraining.java
+++ src/javabasictraining/JavaBasicTraining.java
@@ -15,7 +15,17 @@ public class JavaBasicTraining {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int intVariable = 99;
+        byte byteVariable = 10;
+        double doubleVariable = 5.5;
+        String stringVariable = "Hello Marlow";
+        boolean booleanVariable = false;
+        Object[] objArray = new Object[]{intVariable, byteVariable, doubleVariable, stringVariable, booleanVariable};
+
+        for (Object eachObj : objArray) {
+            System.out.println(eachObj.toString());
+        }
+
     }
-    
+
 }
